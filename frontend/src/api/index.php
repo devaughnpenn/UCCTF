63a502a0b927193b3a634f940e68711f724801e0
TaylorReilly <57732754+TaylorReilly@users.noreply.github.com>
2020-02-01 14:08:46 -0500

    edited code so Backend server deploys properly

--- frontend/src/api/index.php
+++ frontend/src/api/index.php
@@ -1,13 +1,13 @@
 <?php
 // Composer
-require(__DIR__ . '/../../backend/vendor/autoload.php');
+require(__DIR__ . '/../../../backend/vendor/autoload.php');
 
 // Environment
-require(__DIR__ . '/../../backend/env.php');
+require(__DIR__ . '/../../../backend/env.php');
 
 // Yii
-require(__DIR__ . '/../../backend/vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../../backend/vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../../backend/config/web.php');
+$config = require(__DIR__ . '/../../../backend/config/web.php');
 
 (new yii\web\Application($config))->run();
