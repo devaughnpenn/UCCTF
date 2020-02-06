f42cf2c442842425632108853b5301ad3f428b46
TaylorReilly <57732754+TaylorReilly@users.noreply.github.com>
2020-02-06 18:21:53 -0500

    allow team creation from Signup page

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,41 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { WoFlashService } from '@app/wo-module/wo-flash/wo-flash.service';
+import { WoBreadCrumbsService } from '@app/wo-module/wo-breadcrumbs/wo-breadcrumbs.service';
 import { MetaPageService } from '@app/core/meta-page.service';
-import { ApiGeneralService } from '@app/share/api-general.service';
+import { ApiAdminService } from '@app/share/api-admin.service';
+import { TeamTabsComponent } from '@app/admin/teams/team-tabs/team-tabs.component';
 
 @Component({
     templateUrl: 'signup.component.html',
     styleUrls: ['signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-    dataForm = {};
-    formErrors = {};
-
-    isConfirm = 0;
+    profileId: number;
+    dataForm = {
+        status: 0,
+    };
+    dataErrors = {};
 
     constructor (
         private metaPage: MetaPageService,
         private router: Router,
+        private woFlash: WoFlashService,
         private activeRoute: ActivatedRoute,
-        private api: ApiGeneralService,
-    ) {
-        this.metaPage.setTitle('Sign Up');
-    }
+        private breadcrumbs: WoBreadCrumbsService,
+        public api: ApiAdminService,
+    ) {}
 
-    ngOnInit() {
+    ngOnInit () {
         this.activeRoute.params.subscribe(params => {
-            this.isConfirm = parseInt(params['confirm'], 10) || 0;
+            this.profileId = Number(params['id']) || 0;
+            this.metaPage.setTitle(this.profileId === 0 ? 'New' : 'Edit');
+            this.woFlash.show();
+            this.loadData();
         });
     }
 
-    doSubmit() {
-        this.api.send('auth/signup', {}, this.dataForm).then(res => {
+    loadData() {
+        if (this.profileId > 0) {
+            this.api.send('teams/profile', {id: this.profileId}).then(res => {
+                if (res['data'] !== null) {
+                    this.dataForm = res['data'];
+                    TeamTabsComponent.title = res['data']['name'];
+                }
+            });
+        }
+    }
+
+    doSave() {
+        this.woFlash.hide();
+        this.api.send('teams/profile', {id: this.profileId}, this.dataForm).then(res => {
             if (res['errors']) {
-                this.formErrors = res['errors'];
+                setTimeout(() => {
+                    this.dataErrors = res['errors'];
+                    this.woFlash.addError(res['errors']);
+                    this.woFlash.show();
+                }, 100);
             } else {
-                this.formErrors = {};
-                this.router.navigate(['/signup', {confirm: 1}]);
+                if (this.profileId === 0) {
+                    this.woFlash.addMessage('Team creation successful!');
+                    this.router.navigate(['/signup', res['data'].id]);
+                    this.dataErrors = {};
+                } else {
+                    this.dataErrors = {};
+                    setTimeout(() => {
+                        this.woFlash.addMessage('The operation was successfully done!');
+                        TeamTabsComponent.title = this.dataForm['name'];
+                        this.woFlash.show();
+                    }, 100);
+                }
             }
         });
+    }
+
+    onAvatarChanged(event) {
+        if (event.result && event.result.file.id) {
+            this.dataForm['avatar_file_id'] = event.result.file.id;
+        } else if (event.remove === true) {
+            this.dataForm['avatar_file_id'] = null;
+        }
+    }
+
+    doCancel() {
+        this.router.navigate(['quiz/login']);
+        //window.history.back();
+        //event.preventDefault();
     }
 }
