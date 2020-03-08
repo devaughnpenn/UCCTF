cfe83a1779dc00883b1e7258b2c444e222bf8de9
TaylorReilly <57732754+TaylorReilly@users.noreply.github.com>
2020-03-08 13:45:27 -0400

    revise SendEmailAction

--- backend/modules/admin/controllers/events/SendEmailAction.php
+++ backend/modules/admin/controllers/events/SendEmailAction.php
@@ -5,7 +5,9 @@ namespace app\modules\admin\controllers\events;
 use Yii;
 use yii\base\Action;
 use app\models\Teams;
+use app\models\EventTeams;
 use app\models\TeamUsers;
+use app\models\Events;
 use yii\swiftmailer\Mailer;
 
 class SendEmailAction extends Action
@@ -71,12 +73,13 @@ class SendEmailAction extends Action
     /**
      * @inheritdoc
      */
-    public function run()
+    public function run($event_id)
     {
         if (Yii::$app->request->isPost) {
             $emails = [];
             $data = Yii::$app->request->post();
-            $team = Teams::findAll(['team_id' => $data['team_id']]);
+            $event = Events::findOne($event_id);
+            $event_team = EventTeams::findOne(['event_id' => $event_id, 'team_id' => $data['team_id']]);
 
             $users = TeamUsers::find()
                 ->where(['team_id' => $data['team_id']])
@@ -93,39 +96,58 @@ class SendEmailAction extends Action
 
             if (count($emails) > 0) {
                 $host_info = parse_url(Yii::$app->getUrlManager()->getHostInfo());
-                Yii::$app->mailer->compose()
+                Yii::$app->mailer->compose('general/access-to-event', [
+                    'link'       => $data['accessLink'],
+                    'login_link' => $data['loginLink'],
+                    'event'      => $event,
+                    'event_team' => $event_team,
+                ])
                 ->setFrom('no-reply@' . $host_info['host'])
                 ->setTo($emails)
-                ->setSubject('Welcome to Ohio Cyber Range CTF!')
+                ->setSubject('Cincinnati - Access to ' . $event->name)
                 ->send();
             }
 
             return [
                 'code'     => 200,
                 'data'     => $data,
+                'event_id' => $event_id,
                 'users'    => $users,
                 'emails'   => $emails,
             ];
         } else if (Yii::$app->request->isGet) {
             return [
-                'teams' => $this->getTeams(),
+                'teams' => $this->getAssignedTeams($event_id),
             ];
         } else {
             return [];
         }
     }
 
-    private function getTeams()
+    private function getAssignedTeams($event_id)
     {
         $query = Teams::find()
                     ->select([
                         'id'             => 'teams.id',
                         'name'           => 'teams.name',
+                        'event_teams_id' => 'event_teams.id',
+                        'access_key'     => 'event_teams.access_key',
+                        'pin'            => 'event_teams.pin',
                     ])
                     ->asArray();
 
+        $query->leftJoin(
+            'event_teams',
+            implode(' AND ', [
+                'event_teams.team_id = teams.id',
+                'event_teams.event_id = ' . $event_id,
+                'event_teams.status = ' . EventTeams::STATUS_ACTIVE,
+            ])
+        );
+
+        $query->andWhere(['not', ['event_teams.id' => null]]);
+        $query->orderBy(['teams.name' => SORT_ASC]);
+
         return $query->asArray()->all();
     }
-
-
 }
